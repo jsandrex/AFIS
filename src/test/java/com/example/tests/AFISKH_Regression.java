c21ad0db7561e998cb5e6ab12b1b11f35bd10f60
jsramos <jsramos@ACSS-LTP206.domain.aeoncredit.com.ph>
2024-10-31 16:53:05 +0800

    Fixed IE concern ongoing with DE1

--- src/test/java/com/example/tests/AFISKH_Regression.java
+++ src/test/java/com/example/tests/AFISKH_Regression.java
@@ -3,24 +3,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.IOException;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AFISKH_Regression {
 
@@ -56,32 +46,39 @@ public class AFISKH_Regression {
     passwordField.click();
  
     
-    //Home Page
+    //HOME PAGE
     driver.findElement(By.xpath("//*[@id=\"sysLoginWrap\"]/div[2]/input[3]")).click();
     driver.findElement(By.xpath("//*[@id=\"menuToggleBtnWrap\"]/div[1]")).click();
     driver.findElement(By.className("toggleBtnTitle")).click();
 //	driver.findElement(By.cssSelector("#navigation > li.last")).click(); >> Temporary since cannot find in submenu
     driver.get("http://10.100.100.27:9780/AFSCambodiaLatest/DealerUpload.htm");
     
-    //Image Upload
+    //IMAGE UPLAD
     driver.findElement(By.id("FILENAME_0")).sendKeys("C:\\Users\\jsramos\\Pictures\\Signature.png");
     driver.findElement(By.id("FILENAME_1")).sendKeys("C:\\Users\\jsramos\\Pictures\\Signature.png");
     driver.findElement(By.xpath("/html/body/div[4]/form/div/div/input[1]")).click();
+    
  // Select "COPY OF BUSINESS LICENSE" using JavaScript
     WebElement imageTypeDropdown = driver.findElement(By.id("IMAGETYPE_1"));
     ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown);
     Thread.sleep(500);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].value='COPY OF BUSINESS LICENSE';", imageTypeDropdown);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='141';", imageTypeDropdown);
 
-    // Select "APPLICANT PHOTO"
+ // Select "APPLICANT PHOTO"
     WebElement imageTypeDropdown2 = driver.findElement(By.id("IMAGETYPE_2"));
-    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown2);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown2); 
     Thread.sleep(500);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].value='APPLICANT PHOTO';", imageTypeDropdown2);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='136';", imageTypeDropdown2); 
+ // Dropdown Type of Application
+    WebElement TypeOfAppl = driver.findElement(By.xpath("//*[@id=\\\"imgBand\\\"]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/input[1]"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", TypeOfAppl); 
+    Thread.sleep(500);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].id='APPLICATIONTYPE';", TypeOfAppl);
+    
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/input[1]")).click();
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/input[1]")).click();
-    Select TypeOfAppl = new Select(driver.findElement(By.id("APPLICATIONTYPE")));
-    TypeOfAppl.selectByVisibleText("1 : REGULAR");
+    Select TypeOfApplz = new Select(driver.findElement(By.id("APPLICATIONTYPE")));
+    TypeOfApplz.selectByVisibleText("1 : REGULAR");
     driver.findElement(By.xpath("/html/body/div[4]/form/div[3]/table/tbody/tr[2]/td[2]/input[2]")).click();
     LocalDate currentDate = LocalDate.now();
     DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("d");
