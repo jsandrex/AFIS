a821d500d42e064d57d54566b124469d46598b56
jsramos <jsramos@ACSS-LTP206.domain.aeoncredit.com.ph>
2024-10-31 14:09:27 +0800

    test

--- src/test/java/com/example/tests/AFISKH_Regression.java
+++ src/test/java/com/example/tests/AFISKH_Regression.java
@@ -1,14 +1,24 @@
 package com.example.tests;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -19,29 +29,32 @@ public class AFISKH_Regression {
     @BeforeTest
     public void setup() {
 
-        // Set up Edge Driver
-        WebDriverManager.edgedriver().setup();
+    	InternetExplorerOptions options = new InternetExplorerOptions();
+    	options.ignoreZoomSettings();
+    	options.withInitialBrowserUrl("http://10.100.100.27:9780/AFSCambodiaLatest/Login.htm");
+    	driver = new InternetExplorerDriver(options);
+    	driver.manage().window().maximize();
 
-        // Create EdgeOptions to enable IE mode
-        EdgeOptions edgeOptions = new EdgeOptions();
-
-        // Enable IE Mode by adding appropriate arguments
-        edgeOptions.addArguments("inprivate"); // Open in InPrivate mode (optional)
-        edgeOptions.addArguments("ie-mode-force"); // Force IE Mode for this session
-        edgeOptions.addArguments("enable-features=msEdgeIECompat"); // Enable IE compatibility mode
-
-        // Initialize the EdgeDriver with the EdgeOptions
-        driver = new EdgeDriver(edgeOptions);
-
-        // Navigate to the URL that requires IE mode
-        driver.get("http://10.100.100.27:9780/AFSCambodiaLatest/Login.htm");
-        driver.manage().window().maximize();
+//        // Navigate to the URL that requires IE mode
+//        driver.get("http://10.100.100.27:9780/AFSCambodiaLatest/Login.htm");
+//        driver.manage().window().maximize();
     }
     @Test
-    public void AFISKH() {
+    public void AFISKH() throws InterruptedException, IOException{
     //Login Page
     driver.findElement(By.xpath("//*[@id=\"USERID\"]")).sendKeys("SOKMENG");
-    driver.findElement(By.xpath("//*[@id=\"PASSWORD\"]")).sendKeys("SYSADMIN1");
+
+    Thread.sleep(500);
+    // Use JavaScript to set the password
+    WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"PASSWORD\"]"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='SYSADMIN1';", passwordField);
+
+    // Wait again to ensure the input is registered
+    Thread.sleep(500);
+
+    // Focus back on the password field to avoid any loss of input
+    passwordField.click();
+ 
     
     //Home Page
     driver.findElement(By.xpath("//*[@id=\"sysLoginWrap\"]/div[2]/input[3]")).click();
@@ -54,10 +67,17 @@ public class AFISKH_Regression {
     driver.findElement(By.id("FILENAME_0")).sendKeys("C:\\Users\\jsramos\\Pictures\\Signature.png");
     driver.findElement(By.id("FILENAME_1")).sendKeys("C:\\Users\\jsramos\\Pictures\\Signature.png");
     driver.findElement(By.xpath("/html/body/div[4]/form/div/div/input[1]")).click();
-    Select DE1Dropdown = new Select(driver.findElement(By.id("IMAGETYPE_1")));
-    DE1Dropdown.selectByVisibleText("COPY OF BUSINESS LICENSE");
-    Select DE1Dropdown2 = new Select(driver.findElement(By.id("IMAGETYPE_2")));
-    DE1Dropdown2.selectByVisibleText("APPLICANT PHOTO");
+ // Select "COPY OF BUSINESS LICENSE" using JavaScript
+    WebElement imageTypeDropdown = driver.findElement(By.id("IMAGETYPE_1"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown);
+    Thread.sleep(500);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='COPY OF BUSINESS LICENSE';", imageTypeDropdown);
+
+    // Select "APPLICANT PHOTO"
+    WebElement imageTypeDropdown2 = driver.findElement(By.id("IMAGETYPE_2"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown2);
+    Thread.sleep(500);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='APPLICANT PHOTO';", imageTypeDropdown2);
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/input[1]")).click();
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/input[1]")).click();
     Select TypeOfAppl = new Select(driver.findElement(By.id("APPLICATIONTYPE")));
