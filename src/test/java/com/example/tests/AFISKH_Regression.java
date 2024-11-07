f0628baf9af7cf9e6eddbd0df0285c4e77e9545b
jsramos <jsramos@ACSS-LTP206.domain.aeoncredit.com.ph>
2024-11-07 14:18:06 +0800

    AFIS Dealer upload IE mode fix

--- src/test/java/com/example/tests/AFISKH_Regression.java
+++ src/test/java/com/example/tests/AFISKH_Regression.java
@@ -1,14 +1,19 @@
 package com.example.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -33,8 +38,6 @@ public class AFISKH_Regression {
     public void AFISKH() throws InterruptedException, IOException{
     //Login Page
     driver.findElement(By.xpath("//*[@id=\"USERID\"]")).sendKeys("SOKMENG");
-
-    Thread.sleep(500);
     // Use JavaScript to set the password
     WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"PASSWORD\"]"));
     ((JavascriptExecutor) driver).executeScript("arguments[0].value='SYSADMIN1';", passwordField);
@@ -58,38 +61,51 @@ public class AFISKH_Regression {
     driver.findElement(By.id("FILENAME_1")).sendKeys("C:\\Users\\jsramos\\Pictures\\Signature.png");
     driver.findElement(By.xpath("/html/body/div[4]/form/div/div/input[1]")).click();
     
- // Select "COPY OF BUSINESS LICENSE" using JavaScript
+// // Select "COPY OF BUSINESS LICENSE" using JavaScript
+//    WebElement imageTypeDropdown = driver.findElement(By.id("IMAGETYPE_1"));
+//    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown);
+//    Thread.sleep(500);
+//    ((JavascriptExecutor) driver).executeScript("arguments[0].value='141';", imageTypeDropdown);
+//
+// // Select "APPLICANT PHOTO"
+//    WebElement imageTypeDropdown2 = driver.findElement(By.id("IMAGETYPE_2"));
+//    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown2); 
+//    Thread.sleep(500);
+//    ((JavascriptExecutor) driver).executeScript("arguments[0].value='136';", imageTypeDropdown2); 
+//    
+    //USE THIS WHEN NULL VALUE
+    // Select "COPY OF BUSINESS LICENSE" using JavaScript
     WebElement imageTypeDropdown = driver.findElement(By.id("IMAGETYPE_1"));
     ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown);
     Thread.sleep(500);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].value='141';", imageTypeDropdown);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='1';", imageTypeDropdown);
 
  // Select "APPLICANT PHOTO"
     WebElement imageTypeDropdown2 = driver.findElement(By.id("IMAGETYPE_2"));
     ((JavascriptExecutor) driver).executeScript("arguments[0].click();", imageTypeDropdown2); 
     Thread.sleep(500);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].value='136';", imageTypeDropdown2); 
- // Dropdown Type of Application
-    WebElement TypeOfAppl = driver.findElement(By.xpath("//*[@id=\\\"imgBand\\\"]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/input[1]"));
-    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", TypeOfAppl); 
-    Thread.sleep(500);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].id='APPLICATIONTYPE';", TypeOfAppl);
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='0';", imageTypeDropdown2); 
     
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/input[1]")).click();
     driver.findElement(By.xpath("//*[@id=\"imgBand\"]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/input[1]")).click();
-    Select TypeOfApplz = new Select(driver.findElement(By.id("APPLICATIONTYPE")));
-    TypeOfApplz.selectByVisibleText("1 : REGULAR");
-    driver.findElement(By.xpath("/html/body/div[4]/form/div[3]/table/tbody/tr[2]/td[2]/input[2]")).click();
+    
+    //type of application
+    Select TypeOfApplz = new Select(driver.findElement(By.id("APPLICATIONTYPE"))); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='1';", TypeOfApplz);
+    
+    //application date
+    WebElement appldate = driver.findElement(By.xpath("/html/body/div[4]/form/div[3]/table/tbody/tr[2]/td[2]/input[2]"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", appldate);
     LocalDate currentDate = LocalDate.now();
     DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("d");
     String currentDay = currentDate.format(dayFormat);
     driver.findElement(By.xpath("//td[normalize-space()='" + currentDay + "']")).click();
-    Select TypeOfLoan = new Select(driver.findElement(By.id("LOANCODE")));
-    TypeOfLoan.selectByVisibleText("GHP");
-    Select TypeOfLoan1 = new Select(driver.findElement(By.id("CURRENCY")));
-    TypeOfLoan1.selectByVisibleText("USD");
-    Select IDCardNo = new Select(driver.findElement(By.id("IDCARDTYPE")));
-    IDCardNo.selectByVisibleText("1 : ID CARD");
+    Select loancode = new Select(driver.findElement(By.id("LOANCODE"))); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='GHP';", loancode);
+    Select currency = new Select(driver.findElement(By.id("CURRENCY"))); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='USD';", currency);
+    Select IDcardType = new Select(driver.findElement(By.id("IDCARDTYPE"))); 
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value='1';", IDcardType);
     driver.findElement(By.xpath("//*[@id=\"IDCARDNO\"]")).sendKeys("1122334455");
     driver.findElement(By.xpath("/html/body/div[4]/form/div[3]/table/tbody/tr[2]/td[7]/input")).sendKeys("JAMES SELENIUM");
     driver.findElement(By.xpath("/html/body/div[4]/form/div[3]/div[1]/input")).click();
@@ -104,6 +120,7 @@ public class AFISKH_Regression {
 //            driver.quit();
        // }
     //}
-}
+    }
+    
 }
 	
